885d989c96b9f86d0fd1f85947b4d3e336597285
Damien Zinsou PADONOU <39190915+predator229@users.noreply.github.com>
2025-06-08 20:22:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -199,7 +199,7 @@ const connectWithRetry = () => {
     .then(async () => {
       if (process.env.NODE_ENV == 'development') { console.log(`✅ MongoDB connecté avec succès`); }
 
-        if (process.env.NODE_ENV == 'developpment') {
+        if (process.env.NODE_ENV != 'production') {
           await importData();
           console.log('✅ Importation des données terminée.');
         }
